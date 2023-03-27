dec0549616d24ec8aee4ba5f98296f4daa4a9274
Fouzi683 <fouzibkz17@gmail.com>
2023-03-27 01:26:51 +0100

    dbsi dbsi

---
+++ class_matrice.py
@@ -0,0 +1,13 @@
+couleur = ["red","blue","yellow","green"]
+angle=0
+for i in range(7):
+    n=int(input())
+    if n =="red":
+        angle+= 0
+    elif n =="blue":
+        angle+=90
+    elif n =="yellow":
+        angle+=180
+    elif n =="green":
+        angle+=270
+
